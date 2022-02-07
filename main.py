3694b273d9459e4147109e27f7d13f674206a63a
Tedla Raghu <rtedia@woolworths.com.au>
2022-02-07 17:19:47 +1100

    Add requirements.txt file

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from greets import greetings
 from translate import Translator
 import yagmail
+import pandas
 
 translator = Translator(to_lang='te')
 
 yagmail.SMTP(user="tedla.harika@gmail.com", password="Abbhilash")
+
+text = pandas.read_csv('people.csv')
 
 
 for greeting in greetings:
