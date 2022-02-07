ff9ecb3d1d086b2b4c412fdffd7d72686a980569
Tedla Raghu <rtedia@woolworths.com.au>
2022-02-07 17:15:04 +1100

    Add requirements.txt file

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from greets import greetings
 from translate import Translator
+import yagmail
 
 translator = Translator(to_lang='te')
+
+yagmail.SMTP(user="tedla.harika@gmail.com", password="Abbhilash")
 
 
 for greeting in greetings:
